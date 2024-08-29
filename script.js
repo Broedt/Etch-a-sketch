d59e35332217670ec36ad41d213aa4f019014374
Broedt <164227660+Broedt@users.noreply.github.com>
2024-08-29 10:45:30 +0200

    added a progressive darkening effect

--- script.js
+++ script.js
@@ -8,18 +8,35 @@ function divCreator(rowsAndCols){
     let cellSize = containerSize/rowsAndCols -2 +"px";
 for (let i = 0 ; i < rowsAndCols*rowsAndCols; i++){
     let cell = document.createElement("div");
-        cell.style.backgroundColor = ("purple");
+        cell.classList.add("gridDivs");
+        cell.style.backgroundColor = ("rgb(255, 255, 255)");
         cell.style.border = ("1px, solid black");
         cell.style.width = cellSize;
         cell.style.height = cellSize;
-    
+    sketchpad.appendChild(cell);
+
     cell.addEventListener (`mouseover`, function(){
         cell.style.cursor = ("pointer");
-        cell.style.backgroundColor = ("yellow");
-        })
-    sketchpad.appendChild(cell);
+        
+        let currentColor = getComputedStyle(cell).backgroundColor;
+        console.log(currentColor);
+        let newColor = darkenColor(currentColor);
+        cell.style.backgroundColor = newColor;
+    })
 }
 }
+
+// function to darken the color by 10 %
+
+function darkenColor(rgb){
+    const rgbValues = rgb.match(/\d+/g).map(Number);
+    const factor = 255/9 ;
+    const darkendRgb = rgbValues.map(value => Math.floor(value-factor));
+    return `rgb(${darkendRgb.join(", ")})`;
+}
+
+
+
 function changeSize(){
     let userInput = prompt("how many squares per side would you like for your next grid?");
         if (userInput >= 16 && userInput <= 100){
