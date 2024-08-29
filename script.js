a801c9c88ef5385b4d020d1b69d613ccb55e8bc7
Broedt <164227660+Broedt@users.noreply.github.com>
2024-08-29 11:51:56 +0200

    added a function to get a random color(not used yet)

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let content = document.querySelector(".content");
 let sketchpad = document.querySelector(".sketchpad");
 let containerSize = 600;
 let defaultNumber = 16;
-
+const randomColor = getRandomColor()
+console.log (randomColor)
 
 function divCreator(rowsAndCols){
     let cellSize = containerSize/rowsAndCols -2 +"px";
 for (let i = 0 ; i < rowsAndCols*rowsAndCols; i++){
     let cell = document.createElement("div");
-        cell.style.backgroundColor = ("rgb(255, 255, 255)");
+        cell.style.backgroundColor = ("rgb(255, 255, 255");
         cell.style.border = ("1px, solid black");
         cell.style.width = cellSize;
         cell.style.height = cellSize;
@@ -16,7 +17,6 @@ for (let i = 0 ; i < rowsAndCols*rowsAndCols; i++){
 
     cell.addEventListener (`mouseover`, function(){
         cell.style.cursor = ("pointer");
-        
         let currentColor = getComputedStyle(cell).backgroundColor;
         console.log(currentColor);
         let newColor = darkenColor(currentColor);
@@ -32,6 +32,16 @@ function darkenColor(rgb){
     const factor = 255/9 ;
     const darkendRgb = rgbValues.map(value => Math.floor(value-factor));
     return `rgb(${darkendRgb.join(", ")})`;
+}
+
+// fuction to get a random color 
+
+function getRandomColor(){
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 
